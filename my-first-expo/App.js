6af8d18ae83f841ae83fc41d04c98be57f768c13
qjarl5678 <qjarl5678@nate.com>
2022-07-25 15:20:05 +0900

    feat: add myfirstcli

--- my-first-expo/App.js
+++ my-first-expo/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+console.log('Expo DevTools Log Test');
+
 export default function App() {
   return (
     <View style={styles.container}>
